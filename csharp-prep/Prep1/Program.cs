e18bae0caa1b643946354006fbd151afeff69d87
willkjj <willkjj@gmail.com>
2026-04-23 11:01:07 -0600

    Completed prep work 1. Added a few console.writes and a few variables, found with user input

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write(" What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"{lastName}, {firstName} {lastName}");
+
+
+        int x = 10;
+
+        if (x == 20){
+            Console.WriteLine(x);
+        } 
+        else if (x == 20)
+        {
+            Console.WriteLine("llkdfjglkdfj");
+        }
+        else
+        {
+            
+        }
     }
 }
