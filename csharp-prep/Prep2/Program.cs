7e8b5ec260d9fde2623420405c30a5a5812f3ba8
willkjj <willkjj@gmail.com>
2026-04-25 12:31:47 -0600

    Completed prep work 2, with proper grade logic

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Security;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        string valueFromUser = Console.ReadLine();
+        int grade = int.Parse(valueFromUser);
+
+        bool plus = false;
+
+        if (grade % 10 < 5)
+        {
+            plus = false;
+        } else
+        {
+            plus = true;
+        }
+
+        if (grade >= 90)
+        {
+            if (plus == false)
+            {
+                Console.WriteLine("Your grade is an A-");
+            } else
+            {
+                Console.WriteLine("Your grade is an A");
+            }
+        } else if (grade >= 80)
+        {
+            if (plus == false)
+            {
+                Console.WriteLine("Your grade is an B-");
+            } else
+            {
+                Console.WriteLine("Your grade is an B+");
+            }
+        } else if (grade >= 70)
+        {
+            if (plus == false)
+            {
+                Console.WriteLine("Your grade is an C-");
+            } else
+            {
+                Console.WriteLine("Your grade is an C+");
+            }
+        } else if (grade >= 60)
+        {
+            if (plus == false)
+            {
+                Console.WriteLine("Your grade is an D-");
+            } else
+            {
+                Console.WriteLine("Your grade is an D+");
+            }
+        } else
+        {
+            Console.WriteLine("Your grade is an F");
+        }
     }
 }
