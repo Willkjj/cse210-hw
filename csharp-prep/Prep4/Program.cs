f71a68406028903ad5868a31e50f6d525d60d3c6
willkjj <willkjj@gmail.com>
2026-05-02 11:40:49 -0600

    Completed prep work 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,67 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        int input = -1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+        do
+        {
+            Console.Write("Enter a number: ");
+
+            string inputString = Console.ReadLine();
+            input = int.Parse(inputString);
+            
+            if (input != 0)
+            {
+                numbers.Add(input);
+                
+            }
+
+            
+        } while (input != 0);
+
+        int sum = 0;
+        int i = 0;
+        int highest = 0;
+        int smallest = 0;
+
+        foreach (int number in numbers)
+        {
+            i++;
+            sum += number;
+
+            if (number > highest)
+            {
+                highest = number;
+            }
+
+            if (number > 0)
+            {
+                if (number < smallest)
+                {
+                    smallest = number;
+                }
+            }
+
+        }
+
+        numbers.Sort();
+
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {sum / i}");
+        Console.WriteLine($"The highest number is {highest}");
+        Console.WriteLine($"The smallest postive number is {smallest}");
+        Console.WriteLine($"The sorted list is");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
