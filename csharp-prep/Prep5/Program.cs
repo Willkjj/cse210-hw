d0f37c55024ec6c74db60ce66cc9bf86effec0f8
willkjj <willkjj@gmail.com>
2026-05-02 13:23:47 -0600

    Completed prep wrok 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,60 @@ using System;
 
 class Program
 {
+    static void displayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string promptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+    static int promptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+
+        string numberString = Console.ReadLine();
+        int number = int.Parse(numberString);
+
+        return number;
+    }
+    static int promptUserBirthName()
+    {
+        Console.Write("Please enter your birth date: ");
+
+        string numberString = Console.ReadLine();
+        int number = int.Parse(numberString);
+
+        return number;
+    }
+
+    static int squareNumber(int n1)
+    {
+        return n1 * n1;
+    }
+
+    static void displayResult(string user, int number, int date)
+    {
+        int currentDate = DateTime.Now.Year;
+
+        Console.WriteLine($"{user}, the square of your number is {number}");
+        Console.WriteLine($"{user}, you will turn {currentDate - date} this year");
+
+
+    }
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        displayWelcome();
+        string user = promptUserName();
+        int number = promptUserNumber();
+        int date = promptUserBirthName();
+        int squaredNumber = squareNumber(number);
+        displayResult(user, squaredNumber, date);
+
+
     }
 }
