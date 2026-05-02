50b63ff5ac9c229704aac507916e1f859c4f1263
willkjj <willkjj@gmail.com>
2026-05-02 10:20:23 -0600

    Completed prep work 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int num = randomGenerator.Next(1,100);
+
+        int i = 0;
+        int guess = 0;
+
+        do
+        {
+            i++;
+
+            Console.Write("What is the magic number? ");
+            string guessString = Console.ReadLine();
+            guess = int.Parse(guessString);
+            
+            if (guess < num)
+            {
+                Console.WriteLine("Higher!");
+
+            } else if (guess > num)
+            {
+                Console.WriteLine("Lower!");
+
+            } else if (guess == num)
+            {
+                Console.WriteLine($"Congrats! You guessed it in only {i} gueses!");
+            }
+
+            
+        } while (guess != num); 
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,10 +1,32 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
+
+    static void displayGreeting()
+    {
+        Console.WriteLine("heya");
+    }
+    static double addNumbers(int n1, int n2, double n3)
+    {
+        double total = n1 + n2 + n3; 
+        return total;
+    }
     static void Main(string[] args)
     {
+        displayGreeting();
+        double total = addNumbers(2,10,140);
+        Console.WriteLine(total);
+
         //comment.
-        Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine("Hello Sandbox World!");
+        // for (int i = 0; i < 10; i++)
+        // {
+        //     Console.WriteLine(i);
+        // }
+
+        // List<int> data = new List<int>();
+        // data.Add(1);
     }
 }
