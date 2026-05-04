b76ada3a82dcf9b3444c90cca9358abd9f6f9c33
willkjj <willkjj@gmail.com>
2026-05-04 14:47:07 -0600

    Added circleApp project

---
+++ sandbox/circleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Cirlce myCircle = new Cirlce();
+        myCircle.SetRadius(10);
+        Console.WriteLine(myCircle.GetArea());
+    }
+}
---
+++ sandbox/circleApp/circle.cs
@@ -0,0 +1,13 @@
+class Cirlce
+{
+    double _radius;
+    public void SetRadius(double radius)
+    {
+        _radius = radius;
+    }
+
+    public double GetArea()
+    {
+        return Math.PI * _radius *_radius;
+    }
+}
